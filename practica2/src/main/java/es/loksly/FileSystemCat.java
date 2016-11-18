64be5a88ef1fb5ca5edb269b46329897a9f8b7ea
Loksly <loksly@gmail.com>
2016-11-18 17:23:33 +0100

    added better dependency

--- practica2/src/main/java/es/loksly/FileSystemCat.java
+++ practica2/src/main/java/es/loksly/FileSystemCat.java
@@ -10,15 +10,15 @@ import org.apache.hadoop.fs.Path;
 
 public class FileSystemCat {
 	public static void main( String [] args ) throws Exception {
-		String uri = args [0];
-		Configuration conf = new Configuration() ;
-		FileSystem fs = FileSystem.get( URI.create( uri ), conf ) ;
+		String uri = args[0];
+		Configuration conf = new Configuration();
+		FileSystem fs = FileSystem.get(URI.create(uri), conf);
 		FSDataInputStream in = null;
 		try {
-			in = fs.open(new Path( uri ) );
-			IOUtils.copyBytes( in, System.out, 4096, false );
+			in = fs.open(new Path(uri));
+			IOUtils.copyBytes(in, System.out, 4096, false);
 		} finally {
-			IOUtils.closeStream( in ) ;
+			IOUtils.closeStream(in);
 		}
 	}
 }
