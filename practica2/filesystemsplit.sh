627c0fb8a4c3b6d14007f558c2544572933207ae
Loksly <loksly@gmail.com>
2016-11-20 17:22:48 +0100

    error on lib name

--- practica2/filesystemsplit.sh
+++ practica2/filesystemsplit.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wget -q https://github.com/Loksly/tcdm_2016/raw/master/practica2/split/jar/FileSystemSplit.jar
-wget -q https://github.com/Loksly/tcdm_2016/raw/master/practica2/split/lib/FileSystemSplit.jar
+wget -q https://github.com/Loksly/tcdm_2016/raw/master/practica2/split/lib/argparse4j-0.7.0.jar
 classpath=`hadoop classpath`:/opt/yarn/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-client-core-2.7.3.jar
 java -cp $classpath:FileSystemCat.jar:argparse4j-0.7.0.jar es.loksly.FileSystemSplit -f hdfs:///tmp/filesystemcattest -t hdfs:///tmp/half -p 50 -s 50 -p
