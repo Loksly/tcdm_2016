1b54a5b45b0f23222fd6023c457d6944800d9376
Loksly <loksly@gmail.com>
2016-11-20 17:11:35 +0100

    changed filename

--- practica2/filesystemcat.sh
+++ practica2/filesystemcat.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-wget https://github.com/Loksly/tcdm_2016/raw/master/practica2/cat/jar/FileSystemCat.jar
+wget -q https://github.com/Loksly/tcdm_2016/raw/master/practica2/cat/jar/FileSystemCat.jar
 classpath=`hadoop classpath`:/opt/yarn/hadoop/share/hadoop/mapreduce/hadoop-mapreduce-client-core-2.7.3.jar
 hdfs -cp $classpath:FileSystemCat.jar es.loksly.FileSystemCat hdfs:///tmp/filesystemcattest
